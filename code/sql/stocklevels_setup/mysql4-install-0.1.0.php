5507194bac504e759b74d77c3992f3800f7aea11
Martin Shaw <martin.shaw@meanbee.com>
2011-10-04 11:49:14 +0100

    Added exception checking for installer

--- code/sql/stocklevels_setup/mysql4-install-0.1.0.php
+++ code/sql/stocklevels_setup/mysql4-install-0.1.0.php
@@ -1,6 +1,12 @@
 <?php
-$installer = $this;
 
-$installer->installEntities();
+try {
+    $installer = $this;
 
-Mage::getSingleton('adminhtml/session')->addSuccess("Visual Stock Levels Installer Successful: 0.1.0.");
+    $installer->installEntities();
+
+    Mage::getSingleton('adminhtml/session')->addSuccess("Visual Stock Levels Installer Successful: 0.1.0.");
+} catch (Exception $e) {
+    Mage::getSingleton('adminhtml/session')->addError("An error has occurred installing Visual Stock Levels: ". $e->getMessage());
+} 
+
